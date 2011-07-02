d51eade1fe389f138bcd29ece362f61f3d1c7267
mastermate <mastermate@fb2c6975-2c48-0410-9a2f-a18bf4baccf4>
2011-07-02 13:30:26 +0000

    integracion a medio hacer con hibernate

--- trunk/spring3-sample/src/main/java/net/viralpatel/spring3/form/Contact.java
+++ trunk/spring3-sample/src/main/java/net/viralpatel/spring3/form/Contact.java
@@ -1,12 +1,46 @@
 package net.viralpatel.spring3.form;
 
+import java.io.Serializable;
 
-public class Contact {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "CONTACT")
+public class Contact implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4531953881018832745L;
+	
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name = "FIRST_NAME")
 	private String firstname;
+	
+	@Column(name = "LAST_NAME") 
     private String lastname;
+	
+	@Column(name = "EMAIL")
     private String email;
+	
+	@Column(name = "TELEPHONE")
     private String telephone;
     
+    public Integer getId(){
+    	return id;
+    }
+    
+    public void setId(Integer id){
+    	this.id = id;
+    }
     
 	public String getFirstname() {
 		return firstname;
